ea6f657bfc1729efa8989f0e9f2a5b28eb75474d
addai christopher <addaichristopher1@gmail.com>
2023-08-10 21:44:49 -0700

    stderr 59

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
- * Description: A programme to print out to std err
- * Return: Always 0 (Success)
+ *
+ * Return: Always 1 (Success)
  */
-
-int main(void) 
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
