0d708f8da11860a7b8aecbe28dbbd266e16cf4a7
addai christopher <addaichristopher1@gmail.com>
2023-09-01 20:03:00 -0700

    debugged file 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,35 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define UNUSED(x) (void)(x)
-/**
- * StringCheck - checks string
- * @s: string to check
- * Return: boolean
- */
-int StringCheck(char *s)
-{
-	int i = 0;
-
-	for (; s[i] != '\0'; i++)
-	{
-		if (!isdigit(s[i]))
-		{
-			return (0);
-		}
-	}
-	return (1);
-}
 /**
  * main - main function
  * @argc: argumentc
  * @argv: vector of arguments
  *Return: always 0
  */
-int main(int argc, char  *argv[])
-{
-	int i;
-	int result = 0;
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (StringCheck(argv[i]))
-			{
-				result += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", result);
-		return (0);
-	}
-	else
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
+int main(int argc, char *argv[]) {
+    int sum = 0;
+    int i;
 
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++) {
+        int j;
+        for (j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
 }
+
