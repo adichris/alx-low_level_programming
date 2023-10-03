cf763a7879a2211db8c4267c8071c27052aa8b9d
addai christopher <addaichristopher1@gmail.com>
2023-10-03 11:08:46 -0700

    return the length of the list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * list_lem - count number of element in the list
- * @h: a single linked list
- *
- * Return: the number of nodes in the list
- */
-
+* list_len - count the elements of a linked list.
+* @h: a singly linked list
+*
+* Return: the number of nodes
+*/
 
 size_t list_len(const list_t *h)
 {
 	size_t counter = 0;
 
-	whilr (h != NULL)
+	while (h != NULL)
 	{
-		counter ++;
+		++counter;
 		h = h->next;
 	}
 
-	return (counter);
+return (counter);
 }
