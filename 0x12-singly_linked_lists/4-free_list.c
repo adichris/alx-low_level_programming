2c2d4c122b6642f3b7903494546d6950e6f5ca57
addai christopher <addaichristopher1@gmail.com>
2023-10-04 06:32:02 -0700

    fix free linked list error

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,16 +8,6 @@
 * Return: void.
 */
 
-void free_list(list_t *head)#include <string.h>
-#include "lists.h"
-
-/**
-* free_list - frees a singly linked list.
-* @head: a linked list to print
-*
-* Return: void.
-*/
-
 void free_list(list_t *head)
 {
 	list_t *tmp_node;
