ce1baa3190060b7fa2f711d58d1903192a640bfc
addai christopher <addaichristopher1@gmail.com>
2023-10-04 06:38:38 -0700

    print this first before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * The Hare and the Tortoise
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
