5851d2332fd096070d48f251a0571b05e5d14c28
addai christopher <addaichristopher1@gmail.com>
2023-10-04 06:13:27 -0700

    add new node to linked list

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - add a new node of a singly linked list.
+ * @head: a listed list
+ * @str: string characters
+ *
+ * Return: the address of the new element, or NULL  if it failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *tmp_node; /* add a new node */
+	de
+	unsigned int c = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[c++]);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = --c;
+	new_node->next = NULL; /* LAST NODE */
+
+	/* if head is NULL, it is an empty linked list */
+	if (*head == NULL)
+	{
+		(*head) = new_node;
+		return (new_node);
+	}
+
+	tmp_node = *head;
+	while (tmp_node->next)
+		tmp_node = tmp_node->next;
+
+	tmp_node->next = new_node; /* this point to new node */
+
+	return (new_node);
+}
