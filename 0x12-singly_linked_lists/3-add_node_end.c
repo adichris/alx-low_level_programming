471ce6b01452933c5e2c2a48a16dfad0987bab11
addai christopher <addaichristopher1@gmail.com>
2023-10-04 06:29:03 -0700

    modified add node, and addd empty blink list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,7 +12,6 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *tmp_node; /* add a new node */
-	de
 	unsigned int c = 0;
 
 	if (str == NULL)
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+* free_list - frees a singly linked list.
+* @head: a linked list to print
+*
+* Return: void.
+*/
+
+void free_list(list_t *head)#include <string.h>
+#include "lists.h"
+
+/**
+* free_list - frees a singly linked list.
+* @head: a linked list to print
+*
+* Return: void.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *tmp_node;
+	while (head)
+	{
+		tmp_node = head;
+		head = head->next;
+		free(tmp_node->str);
+		free(tmp_node);
+	}
+}
