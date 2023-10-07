6038f66a7db7453135b03e5574ae2d9f8bc84f98
Addai Christopher <addaichristopher1@gmail.com>
2023-10-07 03:06:13 -0700

    fix bug in programm to print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,11 +3,11 @@
  * print_binary - print the binary representation of a number
  * @n: argument to be pass
  *
- * return 0 (sucess)
+ * return 0 (success)
  */
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	printf("%d", n);
+	printf("%d", n & 1);
 }
