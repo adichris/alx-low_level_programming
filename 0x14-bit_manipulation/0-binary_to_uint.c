a1d3c6f8f4bde7a21efb299642f3c36bac4528d7
Addai Christopher <addaichristopher1@gmail.com>
2023-10-06 18:17:27 -0700

    fix betty issues

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,23 @@
 #include "main.h"
+
 /**
  * binary_to_unit - a function to convert binary to unsigned integer
  * @b: binary argument of the number
  * Return: unsigned int
- */
-
-unsigned int binary_to_uint(const char* b)
+*/
 {
-    unsigned int k = 1;
-    unsigned int i = 0;
-    int c;
-    unsigned int len;
+	int i;
+	unsigned int num = 0;
 
-    len = strlen(b);
+	if (b == NULL)
+		return (0);
 
-    for (c = len - 1; c >= 0; c--)
-    {
-        if (b[c] != '0' && b[c] != '1')
-            return (0);
-        if (b[c] == '1')
-        {
-            i += k;
-        }
-        k *= 2;
-    }
-    return (i);
+	for (i = 0; b[i] != '\0'; ++i)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		num = 2 * num + (b[i] - '0');
+	}
+
+	return (num);
 }
